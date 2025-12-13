bf0e1f471adba44799ed79cb3d5bd2c4ffb7ef80
krown2025 <support@fullview.com>
2025-12-13 13:59:58 +0200

    Fix Railway 502: Bind server to 0.0.0.0

--- app.js
+++ app.js
@@ -103,6 +103,6 @@ io.on('connection', (socket) => {
 app.set('io', io);
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
+server.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
